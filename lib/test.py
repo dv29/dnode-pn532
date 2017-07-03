18a61f030930f7ee07cb2e7d9c5343ba6a909807
Deep Vora <deep@infibliss.com>
2017-07-03 11:17:54 +0530

    Typo

--- lib/test.py
+++ lib/test.py
@@ -27,22 +27,22 @@ pn532.SAM_configuration()
 # Main loop to detect cards and read a block.
 print('Waiting for MiFare card...')
 while True:
-# Check if a card is available to read.
-uid = pn532.read_passive_target()
-# Try again if no card is available.
-if uid is None:
-    continue
-print('Found card with UID: 0x{0}'.format(binascii.hexlify(uid)))
-# Authenticate block 4 for reading with default key (0xFFFFFFFFFFFF).
-if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B,
-                                               [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]):
-    print('Failed to authenticate block 4!')
-    continue
-# Read block 4 data.
-data = pn532.mifare_classic_read_block(4)
-if data is None:
-    print('Failed to read block 4!')
-    continue
-# Note that 16 bytes are returned, so only show the first 4 bytes for the block.
-print('Read block 4: 0x{0}'.format(binascii.hexlify(data[:4])))
-time.sleep(15)
+    # Check if a card is available to read.
+    uid = pn532.read_passive_target()
+    # Try again if no card is available.
+    if uid is None:
+        continue
+    print('Found card with UID: 0x{0}'.format(binascii.hexlify(uid)))
+    # Authenticate block 4 for reading with default key (0xFFFFFFFFFFFF).
+    if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B,
+                                                   [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]):
+        print('Failed to authenticate block 4!')
+        continue
+    # Read block 4 data.
+    data = pn532.mifare_classic_read_block(4)
+    if data is None:
+        print('Failed to read block 4!')
+        continue
+    # Note that 16 bytes are returned, so only show the first 4 bytes for the block.
+    print('Read block 4: 0x{0}'.format(binascii.hexlify(data[:4])))
+    time.sleep(15)
