e1f095a332e26732225eb6f1af1637f3df53368c
Deep Vora <deep@infibliss.com>
2017-07-04 14:08:05 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const PythonShell = require("python-shell");
-const EventEmitter = require("events");
-const util = require("util");
+const PythonShell = require('python-shell');
+const EventEmitter = require('events');
+const util = require('util');
 
 function PN532(config) {
   const self = this;
@@ -15,35 +15,35 @@ function PN532(config) {
     },
     config
   );
-
-  this.pyshell = new PythonShell("./lib/pn532.py", {
+  console.log(__dirname);
+  this.pyshell = new PythonShell('./lib/pn532.py', {
     args: [options.SCAN_TIMEOUT, options.CS, options.MOSI, options.MISO, options.SCLK]
   });
 
   // TODO: begin according to config provided
-  this.pyshell.on("message", function(message) {
+  this.pyshell.on('message', function(message) {
     const payload = JSON.parse(message);
     switch (payload.code) {
       case 1:
-        self.emit("initialized", payload.data);
+        self.emit('initialized', payload.data);
         break;
       case 3:
-        self.emit("firmware_version", payload.data);
+        self.emit('firmware_version', payload.data);
         break;
       case 4:
-        self.emit("card_found", payload.data);
+        self.emit('card_found', payload.data);
         break;
       default:
-        console.error("no code found");
+        console.error('no code found');
     }
   });
 
-  this.pyshell.on("error", function(err) {
-    console.error("err: " + err);
+  this.pyshell.on('error', function(err) {
+    console.error('err: ' + err);
   });
 
-  this.pyshell.on("close", function(close) {
-    console.log("Connection to script closed : " + close);
+  this.pyshell.on('close', function(close) {
+    console.log('Connection to script closed : ' + close);
   });
 }
 util.inherits(PN532, EventEmitter);
