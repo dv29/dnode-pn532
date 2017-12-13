078fa611068fd14cfaf96b8ab18664a392a46f09
Deep Vora <deep@infibliss.com>
2017-12-13 12:42:24 +0100

    cleanup

--- index.js
+++ index.js
@@ -3,47 +3,47 @@ const EventEmitter = require('events');
 const util = require('util');
 
 function PN532(config) {
-  const self = this;
+	const self = this;
 
-  const options = Object.assign({
-			INTERFACE: '/dev/ttyS2'
-      SCAN_TIMEOUT: 1.5
-    },
-    config
-  );
+	const options = Object.assign({
+			INTERFACE: '/dev/ttyS2',
+			SCAN_TIMEOUT: 1.5,
+		},
+		config
+	);
 
-  this.pyshell = new PythonShell('pn532.py', {
-    scriptPath: __dirname + '/lib',
-    args: [options.SCAN_TIMEOUT, options.CS, options.MOSI, options.MISO, options.SCLK]
-  });
+	this.pyshell = new PythonShell('pn532.py', {
+		scriptPath: __dirname + '/lib',
+		args: [options.SCAN_TIMEOUT, options.SCLK]
+	});
 
-  // TODO: begin according to config provided
-  this.pyshell.on('message', function(message) {
-    const payload = JSON.parse(message);
-    switch (payload.code) {
-      case 1:
-        self.emit('initialized', payload.data);
-        break;
-      case 3:
-        self.emit('firmware_version', payload.data);
-        break;
-      case 4:
-        self.emit('card_found', payload.data);
-        break;
-      default:
-        console.error('no code found');
-    }
-  });
+	// TODO: begin according to config provided
+	this.pyshell.on('message', function(message) {
+		const payload = JSON.parse(message);
+		switch (payload.code) {
+			case 1:
+				self.emit('initialized', payload.data);
+				break;
+			case 3:
+				self.emit('firmware_version', payload.data);
+				break;
+			case 4:
+				self.emit('card_found', payload.data);
+				break;
+			default:
+				console.error('no code found');
+		}
+	});
 
-  this.pyshell.on('error', function(err) {
+	this.pyshell.on('error', function(err) {
 		self.emit('error', err);
-    console.error('err: ' + err);
-  });
+		console.error('err: ' + err);
+	});
 
-  this.pyshell.on('close', function(close) {
+	this.pyshell.on('close', function(close) {
 		self.emit('close', close);
-    console.log('Connection to script closed : ' + close);
-  });
+		console.log('Connection to script closed : ' + close);
+	});
 }
 util.inherits(PN532, EventEmitter);
 
